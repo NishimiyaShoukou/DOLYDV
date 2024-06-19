5e4d1a53cdbcb40dff71c38809db47693a4bfe36
NishimiyaShouko <61264320+NishimiyaShoukou@users.noreply.github.com>
2024-06-20 01:20:20 +0800

    Update eye.cpp

--- DOLYDV1-1.0/gui/eye/eye.cpp
+++ DOLYDV1-1.0/gui/eye/eye.cpp
@@ -1,10 +1,9 @@
 /*
  * @Author: Thoelc 623624394@qq.com
  * @Date: 2024-04-10 00:08:00
- * @LastEditors: Thoelc 623624394@qq.com
  * @LastEditTime: 2024-04-10 00:10:32
  * @FilePath: \DOLYDV1-1.0\gui\eye\eye.cpp
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 
  */
 #include "eye.h"
 
